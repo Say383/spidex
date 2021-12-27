2714cac75adf2d5b1010b8aa69e1cf6e96f0355b
intrackeable <olesh_@hotmail.com>
2021-12-27 17:15:19 -0300

    About CLI

---
+++ scanner/header.py
@@ -0,0 +1,7 @@
+__author__ = "Alejandro Chilczenko"
+__copyright__ = "Copyright 2021, "
+__credits__ = ["Alejandro Chilczenko"]
+__license__ = "Apache 2.0"
+__version__ = "2.0.0"
+__email__ = "alechilczenko@gmail.com"
+__status__ = "Development"
