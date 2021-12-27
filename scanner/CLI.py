fd9fd7c6072684f3ba17f0b354e8732de9b15555
intrackeable <olesh_@hotmail.com>
2021-12-27 17:08:27 -0300

    Update

--- scanner/CLI.py
+++ scanner/CLI.py
@@ -1,19 +1,13 @@
 #!/usr/bin/env python
-from logo import show
+from logo import banner
 from Commands import get_flags
 from loguru import logger
 from datetime import datetime
 from Ports import TOP_PORTS, COMMON_PORTS
 from MainScanner import Scanner
-import sys
 
-__author__ = "Alejandro Chilczenko"
-__copyright__ = "Copyright 2021, "
-__credits__ = ["Alejandro Chilczenko"]
-__license__ = "Apache 2.0"
-__version__ = "1.0.5"
-__email__ = "alechilczenko@gmail.com"
-__status__ = "Development"
+import sys
+import webtech
 
 #Search country IP blocks in https://www.nirsoft.net/countryip{COUNTRY_CODE}.html
 def get_country_ip_blocks(file):
@@ -41,10 +35,13 @@ def set_port_scan(Discover,top_ports,all_ports):
         Discover.set_ports(TOP_PORTS)
     elif all_ports:
         Discover.set_ports(COMMON_PORTS)
+    else:
+        Discover.set_ports(TOP_PORTS)
 
 def main():
-    print(show())
-    start, end, threads, path, timeout, screenshot, top_ports, all_ports = get_flags()
+    print(banner)
+    print("Developed By: Ale Chilczenko [https://github.com/alechilczenko] \n")
+    start, end, threads, path, timeout, screenshot, top_ports, all_ports, update = get_flags()
     #Verify argument validity
     if  start and end:
         Discover = Scanner(start,end,threads,timeout,screenshot)
@@ -57,9 +54,13 @@ def main():
         end = datetime.now()
         elapsed = end-start
         logger.info("Total execution time: {}".format(elapsed))
+
+    elif update: 
+        webtech.database.update_database(force=True)        
     else:
         logger.info("Please use -h to see all options")
         sys.exit(1)
+
 if __name__ == '__main__':
     try:
         main()
