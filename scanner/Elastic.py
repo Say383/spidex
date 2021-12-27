f2277599b0c563770e5df233214e9abd3aaa570e
intrackeable <olesh_@hotmail.com>
2021-12-27 17:11:02 -0300

    Detect tags with Webtech

--- scanner/Elastic.py
+++ scanner/Elastic.py
@@ -1,9 +1,10 @@
 from datetime import datetime
 from loguru import logger
 from var import CITY, ASN
+from tags import tags
 import geoip2.database
 
-def create_document(ip, ports_dict, banners, hostname, image,default,connection,vnc):
+def create_document(ip, ports_dict, banners, hostname, image,default,connection):
     try:
         keys = list(ports_dict.keys())
         values = list(ports_dict.values())
@@ -24,17 +25,16 @@ def create_document(ip, ports_dict, banners, hostname, image,default,connection,
                 "country": response.country.name,
                 "city": response.city.name,
                 "country_code": response.country.iso_code,
-                "zip_code": response.postal.code,
                 "latitude": response.location.latitude,
                 "longitude": response.location.longitude,
                 "screenshot": image,
-                "vnc": vnc,
                 "anonymous_login": default,
+                "tags": tags(ip,ports_dict),
                 "date": datetime.now().strftime("%d/%m/%Y %H:%M")
             }
-            logger.success("{} | {} | {} | {}".format(ip,keys,response.country.name,response.city.name))
         doc = {k:v for k,v in col.items() if v is not None}
-        
         connection.index(index="devices", document=doc)
     except:
         logger.exception("Exception ocurred:")
+    finally:
+        logger.success("{} | {} | {} | {}".format(ip,keys,response.country.name,response.city.name))
