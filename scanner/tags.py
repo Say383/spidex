0513a629bbcc199b79341be75ed2cc176bc70c0c
intrackeable <olesh_@hotmail.com>
2022-01-04 15:15:23 -0300

    Update: Change JSON webtech report to simple STR and then insert TAGS in Elasticsearch

--- scanner/tags.py
+++ scanner/tags.py
@@ -2,25 +2,22 @@ import webtech
 
 def get_tags(ip,ports):
     try:
-        if "http" in ports:
-            wt = webtech.WebTech(options={'json': True})
-            tags = wt.start_from_url("http://{}:{}".format(ip,ports["http"]), timeout=1)
-            return tags
+        for key, value in ports.items():
+            if "http" in key:
+                if key == "http-alt":
+                    key = "http"
+                wt = webtech.WebTech(options={'json': False})
+                tags = wt.start_from_url("{}://{}:{}".format(key,ip,value), timeout=1)
+                return tags
     except webtech.utils.ConnectionException():
         return None
 
-#Filter JSON report from Webtech App, and convert to list, with only Web Technologies TAGS and versions
+#Filter report from webtech and only return web technologies
 def tags(ip,ports):
-    filt = []
-    update = []
-    result = []
     save = get_tags(ip,ports)
     if save != None:
-        if "tech" in save:
-            for x in save.values(): filt.append(x) 
-            for j in filt[0]: update.append(list(j.values()))
-            for i in update: result.append(list(filter(None, i)))
-            concat = list(map(" ".join,result))
-            return concat
+        techs = save.replace("\t-","").splitlines()
+        filt = [x.strip() for x in techs if not "Detected" in x and not "Target" in x]
+        return filt
     else:
         return None
