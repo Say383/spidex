bf8b141ce27987074edfda6dddf7f34447fccb98
alechilczenko <alechilczenko@gmail.com>
2022-02-15 15:46:58 -0300

    Update: Refactoring code

--- scanner/configs/config.py
+++ scanner/configs/config.py
@@ -1,4 +1,4 @@
 CITY = "../geoip2/CITY/GeoLite2-City.mmdb"
 ASN = "../geoip2/ASN/GeoLite2-ASN.mmdb"
-SLACK_URL = "https://hooks.slack.com/services/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"
+SLACK_URL = ""
 
