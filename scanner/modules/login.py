e167d584f6dc18a28f5744fc6360e80233cdcc8e
alechilczenko <alechilczenko@gmail.com>
2022-02-18 16:47:35 -0300

    Update: Some changes

--- scanner/modules/login.py
+++ scanner/modules/login.py
@@ -4,6 +4,7 @@ from loguru import logger
 import socket
 import ftplib
 
+@logger.catch
 def anonymous_login(ip,ports):
     if 21 in ports:
         try:
@@ -11,7 +12,6 @@ def anonymous_login(ip,ports):
             ftp.connect(ip, 21, timeout=5)
             ftp.login("anonymous","anonymous")
             ftp.quit()
-            logger.info("Detected default login on: {}".format(ip))
             return True
             
         except (ftplib.error_temp, ftplib.error_perm, socket.timeout, EOFError, ConnectionRefusedError, OSError):
