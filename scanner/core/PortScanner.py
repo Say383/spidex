7fbcd6e3213d390fab8df2558df769750e99ee8b
alechilczenko <alechilczenko@gmail.com>
2022-02-18 16:43:38 -0300

    Update: Logger.Catch decorator

--- scanner/core/PortScanner.py
+++ scanner/core/PortScanner.py
@@ -1,9 +1,6 @@
 import socket
 from loguru import logger
 
-#logger.add(enqueue=True, level="DEBUG", backtrace=True, diagnose="True")
-
-#Basic exception handling
 class Port_Scanner():
     def __init__(self, ip):
         self.ip = ip
@@ -11,6 +8,7 @@ class Port_Scanner():
         self.banners = []
         self.ports = []
 
+    @logger.catch
     def start(self,timeout,ports):
         for port in ports:
 
@@ -23,35 +21,35 @@ class Port_Scanner():
                 response = target.connect_ex((self.ip, port))
 
                 if  response == 0:
+
                     self.ports.append(port)
 
-                    self.banners.append(self.get_banner(target,port))
+                    self.banners.append(self.get_banner(target))
                     try:
                         self.hostname = socket.gethostbyaddr(self.ip)[0]
                     except socket.herror:
                         self.hostname = None
 
+            except socket.timeout:
+                logger.debug(f"{self.ip} Socket timed out")
 
+            except ConnectionResetError:
+                logger.debug(f"{self.ip} Connection reseted by host")
 
-            except socket.timeout: logger.debug("{} | Socket timed out".format(self.ip))
-            except ConnectionResetError: logger.debug("{} | Connection reseted by host".format(self.ip))
-            except: logger.exception("Exception ocurred: ")
             finally:
                 target.close()
 
-    def get_banner(self,target,port):
-
-        ports = [80,8080,8081,8000,8001,82,8888]
-
-        if port in ports:
-            byte = "GET / HTTP/1.1\r\nHost: {}\r\nAccept: text/html\r\nConnection: close\r\n\r\n".format(self.ip)
-            data = str.encode(byte)
-            target.sendall(data)
+    def get_banner(self,target):
+       
+        byte = "GET / HTTP/1.1\r\nHost: {}\r\nAccept: text/html\r\nConnection: close\r\n\r\n".format(self.ip)
+        data = str.encode(byte)
+        target.sendall(data)
 
         return target.recv(4096).decode("utf-8", errors='ignore')
+
 
     def contain_results(self):
         if self.ports: return True
 
     def get_results(self):
-        return self.ports, self.banners, self.hostname
+        return self.banners, self.hostname, self.ports
