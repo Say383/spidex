8d26daebdc093cf403453a7dc7af2c7ddc611154
alechilczenko <alechilczenko@gmail.com>
2022-02-19 18:03:48 -0300

    Fix: Issue in searching database

--- scanner/modules/document.py
+++ scanner/modules/document.py
@@ -3,47 +3,48 @@ from loguru import logger
 
 from modules.login import anonymous_login
 from configs.config import CITY, ASN
-from modules.tags import tags
 
+import geoip2.errors
 import geoip2.database
-
 from colorama import Fore
 
-def create_document(ip, banners, hostname,ports):
+@logger.catch
+def search_geolocation(ip):
     try:
-        tag = tags(ip,banners,ports)
-
         with geoip2.database.Reader(CITY) as geo_reader:
             response = geo_reader.city(ip)
         with geoip2.database.Reader(ASN) as asn_reader:
             resp = asn_reader.asn(ip)
+            return resp.autonomous_system_organization, resp.autonomous_system_number, response.country.name, response.city.name, response.country.iso_code, response.location.latitude, response.location.longitude
 
-            col = {
-                "ip": ip,
-                "org": resp.autonomous_system_organization,
-                "asn": resp.autonomous_system_number,
-                "banners": banners,
-                "ports": ports,
-                "hostname": hostname,
-                "country": response.country.name,
-                "city": response.city.name,
-                "country_code": response.country.iso_code,
-                "latitude": response.location.latitude,
-                "longitude": response.location.longitude,
-                "anonymous_login": anonymous_login(ip,ports),
-                "tags": tag,
-                "date": datetime.now().strftime("%d/%m/%Y %H:%M")
+    except geoip2.errors.AddressNotFoundError:
+        return None
 
-            }
-        doc = {k:v for k,v in col.items() if v is not None}
-    except:
-        logger.exception("Exception ocurred:")
-    finally:
-        #Formatting output, removig None values
-        if tag != None:
-            output = f"{ip} {Fore.LIGHTMAGENTA_EX}{ports} {Fore.BLUE}{tag} {Fore.CYAN}{response.country.name} {Fore.WHITE}{response.city.name}"
-        else:
-            output = f"{ip} {Fore.LIGHTMAGENTA_EX}{ports} {Fore.CYAN}{response.country.name} {Fore.WHITE}{response.city.name}"
+def create_document(ip,banners, hostname,ports,tags):
 
-        logger.success(output)
+    org,asn,country,city,iso_code,lat,long = search_geolocation(ip)
+    col = {
+            "ip": ip,
+            "org": org,
+            "asn": asn,
+            "banners": banners,
+            "ports": ports,
+            "hostname": hostname,
+            "country": country,
+            "city": city,
+            "country_code": iso_code,
+            "latitude": lat,
+            "longitude": long,
+            "anonymous_login": anonymous_login(ip,ports),
+            "tags": tags,
+            "date": datetime.now().strftime("%d/%m/%Y %H:%M")
+        }
+    doc = {k:v for k,v in col.items() if v is not None}
+    #Formatting output, removig None values
+    if tags != None:
+        output = f"{ip} {Fore.LIGHTMAGENTA_EX}{ports} {Fore.BLUE}{tags} {Fore.CYAN}{country} {Fore.WHITE}{city}"
+    else:
+        output = f"{ip} {Fore.LIGHTMAGENTA_EX}{ports} {Fore.CYAN}{country} {Fore.WHITE}{city}"
+
+    logger.success(output)
     return doc
