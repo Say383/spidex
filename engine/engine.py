9ba1a7482df9dbdd26f06762fb3d7f2af0ec4abc
alechilczenko <alechilczenko@gmail.com>
2022-05-12 22:33:05 -0300

    Update: Some small changes

--- engine/engine.py
+++ engine/engine.py
@@ -1,71 +1,57 @@
 #!/usr/bin/env python
 from loguru import logger
-from modules.logo import title
-from modules.Commands import get_flags
-from configs.Ports import TOP_PORTS, COMMON_PORTS
-from core.MainScanner import Scanner
-from modules.mongo import insert_data
-from modules.slack import send_message
-from modules.save  import save_json
-
-from configs import log
+from core.ranges import single_range, multiple_ranges
+from core.parser import get_flags
+from core.threadscan import Threadscan
+from core.logo import title
+import config
 import sys
 
-#Search country IP blocks in https://www.nirsoft.net/countryip{COUNTRY_CODE}.html
-def get_country_ip_blocks(file):
-    total = []
-    with open(file, 'r') as flist:
-        blocks = list(filter(None,flist.read().splitlines()))
-    for ip in blocks:
-        line = ip.split(",")
-        block = [line[0],line[1]]
-        total.append(block)
-    return total
-
-def launch_scanner(start,end,threads,timeout,top_ports,all_ports,slack,custom,save):
-    Discover = Scanner(start,end)
+def launch_scanner(targets,threads,timeout,top_ports,all_ports,custom):
+    Discover = Threadscan(targets)
+    Settings = config.Config()
 
     if top_ports:
-        Discover.set_ports(TOP_PORTS)
+        Discover.set_ports(Settings.top_ports)
     elif all_ports:
-        Discover.set_ports(COMMON_PORTS)
+        Discover.set_ports(Settings.ports)
     elif custom:
         Discover.set_ports(custom)
+    else:
+        logger.info("Please set port scan mode")
+        exit()
 
     Discover.start_threads(threads,timeout)
-    results = Discover.slack_message()
-    send_message(results,slack)
-
+    
     devices = Discover.get_devices()
-    if devices:
-        if save == "mongodb":
-            insert_data(devices)
-        elif save == "json":
-            logger.info("Saving results at {}".format(save_json(devices)))
 
 def main():
+
     print(title)
-    start, end, threads, path, timeout, top_ports, all_ports, custom, slack, save, logs = get_flags()
+    
+    iprange,threads,path,timeout,top_ports,all_ports,custom,logs = get_flags()
+
+    logger.remove(0)
+
+    logger.add(sys.stderr,colorize=True,format="<blue>{time:HH:mm:ss}</blue> | <level>{level: <8}</level> | <level>{message}</level>", enqueue=True)
 
     if logs:
         logger.add("logs/{time}.log", enqueue=True)
         
-    if  start and end:
-        launch_scanner(start,end,threads,timeout,top_ports,all_ports,slack,custom,save)
+    if  iprange:
+        targets = single_range(iprange)
+        launch_scanner(targets,threads,timeout,top_ports,all_ports,custom)
 
     elif path:
-        countries = get_country_ip_blocks(path)
-        for ip in countries:
-            start = ip[0]
-            end = ip[1]
-            launch_scanner(start,end,threads,timeout,top_ports,all_ports,slack,custom,save)
+        targets = multiple_ranges(path)
+        launch_scanner(targets,threads,timeout,top_ports,all_ports,custom)
     else:
         logger.info("Please use -h to see all options")
-        sys.exit(1)
+        exit()
 
 if __name__ == '__main__':
     try:
         main()
     except KeyboardInterrupt:
         logger.info("You pressed CRTL+C")
-        sys.exit(1)
+        exit()
