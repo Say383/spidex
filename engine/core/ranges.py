69fb34b53754842e237e3a37276a286d8f05d2b9
alechilczenko <alechilczenko@gmail.com>
2022-05-12 22:32:14 -0300

    Update: Detects the type of range

--- engine/core/ranges.py
+++ engine/core/ranges.py
@@ -1,7 +1,59 @@
 from ipaddress import ip_address
+from loguru import logger
+import ipaddress
+import queue
+
+'''
+Some functions to validate ip address ranges received as arguments.
+Detects the type of range, for example if it is CIDR and generates a queue with all the targets to send to the Threadscan object.  
+'''
 
 def get_ranges(start,end):
     #Get total of ip addresses
     start_int = int(ip_address(start).packed.hex(), 16)
     end_int = int(ip_address(end).packed.hex(), 16)
     return [ip_address(ip).exploded for ip in range(start_int, end_int)]
+
+def get_total_ip_ranges(file):
+    total = []
+
+    with open(file, 'r') as flist:
+        blocks = list(filter(None, flist.read().splitlines()))
+    for ip in blocks:
+        targets = detect_range_type(ip)
+
+        for t in targets:
+            total.append(t)
+
+    return total
+
+def put_targets_in_queue(targets):
+    q = queue.Queue()
+    for t in targets:
+        q.put(t)
+    return q
+
+def single_range(iprange):
+
+    total = detect_range_type(iprange)
+    return put_targets_in_queue(total)
+
+def multiple_ranges(file):
+
+    total = get_total_ip_ranges(file)
+    result = put_targets_in_queue(total)
+    return result
+
+def detect_range_type(iprange):
+    if "," in iprange:
+        ranges = iprange.split(",")
+        result = get_ranges(ranges[0],ranges[1])
+    elif "/" in iprange:
+        result = get_cidr(iprange)
+    else:
+        logger.info("Invalid target")
+        exit()
+    return result
+
+def get_cidr(iprange):
+    return [str(ip) for ip in ipaddress.IPv4Network(iprange)]
